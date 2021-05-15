84a63908855f9b591521ed1fdda310f6a4ebdfff
Ben Rucker <brucker117@gmail.com>
2021-05-15 19:54:20 -0400

    add watch vue app
    Co-authored-by: Ethan Fidler <ethancf1999@gmail.com>

--- dub/dub.js
+++ dub/dub.js
@@ -1,1 +1,79 @@
+jsonPath = "catalog.json"
+
+const player = {
+    data() {
+        return {
+            title: "",
+            releaseDate: "",
+            videoURL: "",
+            thumbnailURL: "",
+        }
+    },
+    async mounted() {
+        // populate instance variables
+        const id = getID();
+        const data = await getVideoDataFromID(id);
+
+        this.title = data["title"];
+        this.releaseDate = constructDate(data["releaseDate"]);
+        this.videoURL = constructVideoURL(id);
+        this.thumbnailURL = constructThumbnailURL(id);
+
+        document.title = "Watching " + this.title
+    }
+}
+
+const app = Vue.createApp(player).mount('#playerApp')
+
+function getID() {
+    let id = getIDFromURL()
+    if (id === null) {
+        window.location.replace("../dub");
+    }
+    return id
+}
+
+function getIDFromURL() {
+    let uri = window.location.search.substring(1);
+    let params = new URLSearchParams(uri);
+    let id = params.get("v");
+    return id;
+}
+
+async function getVideoDataFromID(id) {
+    let outEp;
+    const request = async () => {
+        const response = await fetch(jsonPath);
+        const json = await response.json();
+        const season = json[getSeason(id)]
+        outEp = getEpisodeFromSeason(season, id);
+    }
+    await request();
+    return outEp
+}
+
+function getEpisodeFromSeason(season, id) {
+    for (const epIndex in season) {
+        const episode = season[epIndex];
+        if (episode["id"] === id) {
+            return episode;
+        }
+    }
+}
+
+function constructVideoURL(id) {
+    return "resources/videos/" + getSeason(id) + "/" + id + ".mp4"
+}
+
+function getSeason(id) {
+    return id.split('-')[0];
+}
+
+function constructThumbnailURL(id) {
+    return "resources/thumbnails/" + id + ".png"
+}
+
+function constructDate(date) {
+    return joinDate = new Date(date).toLocaleDateString('en-US');
+}
 

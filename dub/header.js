1dba4bdf37e156d02b89d677a31a9d2f7fcda4a0
Ben Rucker <brucker117@gmail.com>
2021-05-19 19:17:29 -0400

    move header to own files

--- dub/header.js
+++ dub/header.js
@@ -4,3 +4,18 @@ links = document.querySelectorAll("#navbarSupportedContent .btn")
 links.forEach(link => {
     link.classList.add(colors[col])
 });
+
+var head = document.getElementsByTagName('HEAD')[0]; 
+  
+// Create new link Element
+var link = document.createElement('link');
+
+// set the attributes for link element 
+link.rel = 'stylesheet'; 
+
+link.type = 'text/css';
+
+link.href = 'header.css'; 
+
+// Append link element to HTML head
+head.appendChild(link); 

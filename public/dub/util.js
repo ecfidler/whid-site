7f3ea8c74b8204794caea6ab520f1c6000b848ad
Ben Rucker <brucker117@gmail.com>
2021-05-21 21:53:52 -0400

    Refactor util.js

--- public/dub/util.js
+++ public/dub/util.js
@@ -16,6 +16,67 @@ async function loadCatalog() {
     await request();
 }
 
+function getSeason(epid) {
+    return getSeasonByID(getSeasonID(epid))
+}
+
+function getSeasonByID(seasonID) {
+    return getSeasons()[seasonID]
+}
+
+function getSeasonID(id) {
+    return id.split('-')[0];
+}
+
+function getSeasons() { // called outside of file
+    return catalog["seasons"]
+}
+
+function getFeaturedVideo() {
+    let [id, desc] = getFeaturedVideoData();
+    let data = getVideoDataFromID(id);
+    return [data, desc]
+}
+
+function getFeaturedVideoData() {
+    return [catalog['featured']["id"], catalog["featured"]["description"]]
+}
+
+function getVideoDataFromID(epid) {
+    const season = getSeason(epid);
+    const episodes = season["episodes"];
+    return getEpisodeFromList(episodes, epid);
+}
+
+function getEpisodeFromList(episodes, epid) {
+    for (const episode of episodes) {
+        if (episode["id"] === epid) {
+            return episode;
+        }
+    }
+    throw new VideoIDError("Video ID not found in catalog")
+}
+
+function constructWatchURL(epid) {
+    return "watch.html?v=" + epid
+}
+
+function constructVideoURL(epid) {
+    return "resources/videos/" + getSeasonID(epid) + "/" + epid + ".mp4"
+}
+
+function constructThumbnailURL(id) {
+    return "resources/thumbnails/" + id + ".png"
+}
+
+function constructDate(date) {
+    return date;
+}
+
+function goToGallery() {
+    window.location.replace("../dub/?error=true");
+}
+
 function getErrorFromURL() {
     return Boolean(getParamFromURL("error"))
 }
@@ -42,62 +103,4 @@ function getParamsFromURL() {
     let uri = window.location.search.substring(1);
     let params = new URLSearchParams(uri);
     return params;
-}
-
-function getVideoDataFromID(id) {
-    const season = getSeason(id);
-    const episodes = season["episodes"];
-    return getEpisodeFromList(episodes, id);
-}
-
-function getSeasons() {
-    return catalog["seasons"]
-}
-
-function getFeaturedVideo() {
-    let [id, desc] = getFeaturedVideoData();
-    let data = getVideoDataFromID(id);
-    return [data, desc]
-}
-
-function getFeaturedVideoData() {
-    return [catalog['featured']["id"], catalog["featured"]["description"]]
-}
-
-function getEpisodeFromList(episodes, id) {
-    for (const episode of episodes) {
-        if (episode["id"] === id) {
-            return episode;
-        }
-    }
-    throw new VideoIDError("Video ID not found in catalog")
-}
-
-function constructWatchURL(id) {
-    return "watch.html?v=" + id
-}
-
-function constructVideoURL(id) {
-    return "resources/videos/" + getSeasonID(id) + "/" + id + ".mp4"
-}
-
-function getSeasonID(id) {
-    return id.split('-')[0];
-}
-
-function getSeason(id) {
-    let seasonID = getSeasonID(id);
-    return catalog["seasons"][seasonID]
-}
-
-function constructThumbnailURL(id) {
-    return "resources/thumbnails/" + id + ".png"
-}
-
-function constructDate(date) {
-    return date;
-}
-
-function goToGallery() {
-    window.location.replace("../dub/?error=true");
 }

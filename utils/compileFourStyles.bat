78abbd3fca122d689df713958c72cc2f6bd62404
Ben Rucker <brucker117@gmail.com>
2021-05-24 15:38:47 -0400

    Init dynamic style switching

---
+++ utils/compileFourStyles.bat
@@ -0,0 +1,6 @@
+cd ..
+
+sass scss\set-orange.scss public\dub\style\whid-orange.css
+sass scss\set-pink.scss public\dub\style\whid-pink.css
+sass scss\set-purple.scss public\dub\style\whid-purple.css
+sass scss\set-cyan.scss public\dub\style\whid-cyan.css

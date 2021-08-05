ee01e8dd128b7f26f59579de18f6d165d61a544e
benrucker <brucker117@gmail.com>
2021-08-05 10:07:08 -0400

    Add stream page

---
+++ public/stream.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>STREAM</title>
+      <meta http-equiv = "refresh" content = "0; url = https://open.spotify.com/track/7qiZfU4dY1lWllzX7mPBI3?si=2916938f23564373" />
+   </head>
+   <body style="background-color: black;">
+      <p style="color: white; font-size: 10em; text-align: center;">STREAM</p>
+   </body>
+</html>
